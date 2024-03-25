42082d704922aaf8820916e8d128f3c0d5bac8c6
Mircea MATEI <matei_tm@yahoo.com>
2024-03-25 03:17:11 +0200

    Add earthly build manager

--- module-1/build.gradle.kts
+++ module-1/build.gradle.kts
@@ -2,3 +2,10 @@ plugins {
     id("java")
     id("io.quarkus")
 }
+val quarkusPlatformGroupId: String by project
+val quarkusPlatformArtifactId: String by project
+val quarkusPlatformVersion: String by project
+
+dependencies {
+    implementation(enforcedPlatform("${quarkusPlatformGroupId}:${quarkusPlatformArtifactId}:${quarkusPlatformVersion}"))
+}
